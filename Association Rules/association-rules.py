4ccb1849718cc2d0f1cc90afc00ecb2d03ff95e9
Kritsana CHAIKEAW <kritsana.chai@ku.th>
2018-03-01 22:29:00 +0700

    lactice level 1

--- Association Rules/association-rules.py
+++ Association Rules/association-rules.py
@@ -11,4 +11,11 @@ for d in data:
     items = np.union1d(items, d)
 
 items = np.delete(items, np.where(items=='nan'))
-print(items.shape)
+level_1 = np.ndarray(shape=(0, 2))
+print(level_1.shape)
+
+for item in items:
+    idxes = np.where(data == item)[0]
+    level_1 = np.append(level_1, [[item, len(idxes)]], axis=0)
+
+print(level_1)
